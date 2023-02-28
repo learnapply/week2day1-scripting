26edc7acbc82fa20b5f4b09d028048d7f10a20b1
abydenes <schoolreef2@protonmail.com>
2023-02-28 21:27:18 +0300

    Initial commit

---
+++ providers.js
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import { ethers } from "ethers";
+
+const infuraUrl = `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`;
+const provider = new ethers.providers.JsonRpcProvider(infuraUrl);
+
+console.log("current block number is", provider.getBlockNumber());
